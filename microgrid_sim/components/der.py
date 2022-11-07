9aba9932f09c6f7cca1c9c032d3f48750400941b
tanelikor <81684354+tanelikor@users.noreply.github.com>
2022-11-07 19:49:07 +0200

    Initial commit

--- microgrid_sim/components/der.py
+++ microgrid_sim/components/der.py
@@ -1,1 +1,11 @@
-"""Model for a Distributed Energy Resource (DER), e.g. a set of wind turbines."""
+import csv
+
+
+class DER:
+    """
+    Simulation for Distributed Energy Resource (DER), e.g. a set of wind turbines.
+    This implementation simply reads data from the provided csv file and return it one value at a time
+    when requested.
+    """
+    def __init__(self, data_file: str):
+        pass
