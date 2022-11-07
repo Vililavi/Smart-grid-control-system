b58020b9f0c8efc9626cebffd7168428450b0d12
tanelikor <81684354+tanelikor@users.noreply.github.com>
2022-11-07 20:19:29 +0200

    Simple implementation that ignores timestamps.

--- microgrid_sim/components/csv_data_parser.py
+++ microgrid_sim/components/csv_data_parser.py
@@ -5,5 +5,16 @@ class CSVDataParser:
     """Parses timestamped data from a csv file."""
 
     def __init__(self, data_file: str):
+        """
+        :param data_file: Path to the csv file containing the data.
+        """
         self._rows = []
+        self._data: list[float] = []
+        with open(data_file, newline="", encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            for row in reader:
+                self._rows.append(row)
+                self._data.append(float(row[-1]))
 
+    def get_data(self, idx: int) -> float:
+        return self._data[idx]
--- microgrid_sim/components/der.py
+++ microgrid_sim/components/der.py
@@ -1,11 +1,14 @@
-import csv
+from microgrid_sim.components.csv_data_parser import CSVDataParser
 
 
-class DER:
+class DER(CSVDataParser):
     """
     Simulation for Distributed Energy Resource (DER), e.g. a set of wind turbines.
     This implementation simply reads data from the provided csv file and return it one value at a time
     when requested.
     """
     def __init__(self, data_file: str):
-        pass
+        super().__init__(data_file)
+
+    def get_generated_energy(self, idx: int) -> float:
+        return self.get_data(idx)
