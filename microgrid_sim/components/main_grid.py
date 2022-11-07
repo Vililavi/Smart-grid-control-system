3a2e3f3661b4929a799faeca866c612f52567811
tanelikor <81684354+tanelikor@users.noreply.github.com>
2022-11-07 20:40:57 +0200

    Initial commit

--- microgrid_sim/components/main_grid.py
+++ microgrid_sim/components/main_grid.py
@@ -1,1 +1,5 @@
-"""Model for the main electricity grid."""
+
+
+class MainGrid:
+    """Model for the main electricity grid."""
+    # TODO: Load files (as with DER) and keep track of sold/purchased energy and its price.
