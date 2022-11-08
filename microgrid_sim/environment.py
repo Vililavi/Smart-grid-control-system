778d8257487635a835efd0d0a4f58f80901104cf
tanelikor <81684354+tanelikor@users.noreply.github.com>
2022-11-09 00:50:47 +0200

    Sketching

---
+++ microgrid_sim/environment.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, field, InitVar
+from itertools import count
+
+from microgrid_sim.components.main_grid import MainGrid
+from microgrid_sim.components.der import DER
+from microgrid_sim.components.ess import ESS
+from microgrid_sim.components.price_responsive import PriceResponsiveLoad
+from microgrid_sim.components.tcl import TCL
+from microgrid_sim.components.tcl_aggregator import TCLAggregator
+
+
+@dataclass
+class Environment:
+    """Environment that the EMS agent interacts with, combining the environment together."""
+    main_grid: MainGrid
+    der: DER
+    ess: ESS
+    res_loads: list[PriceResponsiveLoad]
+    tcl_aggregator: TCLAggregator = field(init=False)
+    tcls: InitVar[list[TCL]]
+    _timestep_counter: count
+    start_time_idx: InitVar[int]
+
+    def __post_init__(self, tcls: list[TCL], start_time_idx: int):
+        self.tcl_aggregator = TCLAggregator(tcls)
+        self._timestep_counter = count(start_time_idx)
+
+    def tick(self):
+        """
+        Simulate one (next) timestep with the given control actions.
+        Returns generated profit (reward) and the new state of the environment.
+        """
+
+    def _cover_energy_deficiency(self, energy: float, priority: str) -> float:
+        """Cover energy deficiency from ESS and/or MainGrid. Returns cost."""
+        # TODO: Could be essentially what is now in energy_deficiency.py
+
+    def _handle_excess_energy(self, energy: float, priority: str) -> float:
+        """Store excess energy to the ESS or sell it to the MainGrid. Returns profit."""
+        # TODO: Could be essentially what is now in energy_excess.py
