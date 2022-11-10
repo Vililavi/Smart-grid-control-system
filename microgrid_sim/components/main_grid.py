f8328dd44b597cb35c65a86351bc5237055867ca
tanelikor <81684354+tanelikor@users.noreply.github.com>
2022-11-10 22:30:17 +0200

    Include transmission costs

--- microgrid_sim/components/main_grid.py
+++ microgrid_sim/components/main_grid.py
@@ -26,9 +26,15 @@ class MainGrid:
         return float(self._down_prices.iloc[idx][-1])
 
     def get_bought_cost(self, bought_energy: float, price_idx: int) -> float:
-        """Returns the cost of energy bought from the main electricity grid at a given hour."""
-        return bought_energy * self.get_up_price(price_idx)
+        """
+        Returns the cost of energy bought from the main electricity grid at a given hour,
+        including transmission cost.
+        """
+        return bought_energy * (self.get_up_price(price_idx) + self.imp_transmission_cost)
 
-    def get_sold_cost(self, sold_energy: float, price_idx: int) -> float:
-        """Returns the cost of energy sold to the main electricity grid at a given hour."""
-        return sold_energy * self.get_down_price(price_idx)
+    def get_sold_profit(self, sold_energy: float, price_idx: int) -> float:
+        """
+        Returns the profit gained by selling energy to the main electricity grid at a given hour.
+        Accounts for the transmission cost.
+        """
+        return sold_energy * (self.get_down_price(price_idx) - self.exp_transmission_cost)
